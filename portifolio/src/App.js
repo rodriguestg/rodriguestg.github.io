77148818e9a4ac39121f9091f08f84ead28954c1
rodriguestg <grs.ibr@gmail.com>
2022-12-31 02:25:46 -0300

    Alterações iniciais de transformação em React

---
+++ portifolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import InitialPage from './pages/InitalPage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <InitialPage />
+    </div>
+  );
+}
+
+export default App;
